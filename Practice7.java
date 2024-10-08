f2508ea06c0a63c1f1ead95e128b1fb3f5d67daa
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-10-08 17:16:16 +0530

    Create Practice7.java

---
+++ Practice7.java
@@ -0,0 +1,11 @@
+//Q.16 Tina wants a new carpet for her rectangular dining room. 
+//Her dining room is a 5 meters by 7 meters rectangle. 
+//How much carpet does she need to buy to cover her entire dining room
+public class Practice7 {
+  public static void main(String[] args) {
+    int length=5;
+    int breadth=7;
+    int area=length*breadth;
+    System.out.println("a Carpet that tina needs to buy to cover her entire dining room is : "+area);
+  }  
+}
