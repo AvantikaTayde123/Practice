27035c015a00709a74bf3d50be1ac76fa6caaa57
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-10-09 23:28:36 +0530

    Create Practice10.java

---
+++ Practice10.java
@@ -0,0 +1,10 @@
+//A carpet is 5 m long and 4 m wide. Find its price at the rate of $ 205 per square meter.
+public class Practice10 {
+    public static void main(String[] args) {
+        int length=5;
+        int breadth=4;
+        int area=length*breadth;
+        int price=area*205;
+        System.out.println("The price of carpet is "+price+" at the rate of $ 205 per square meter");
+    }
+}
