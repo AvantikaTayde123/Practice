f9f04968618dc7b18a63941d43b726ef36755dfd
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-10-09 23:27:05 +0530

    Create Practice8.java

---
+++ Practice8.java
@@ -0,0 +1,15 @@
+//Luci is making a display board for the school exhibition.
+//The display board is a 3 m by 2 m rectangle. He needs to add a 
+//ribbon border around the entire display board. What is the length of ribbon that he needs?
+
+
+public class Practice8 {
+    public static void main(String[] args) {
+        int length=3;
+        int breadth=2;
+        int perameter;
+        perameter=2*(length+breadth);
+        System.out.println("The length of ribbon Luci needs to add to enitire display board is : "+perameter+" meters");
+    }
+    
+}
