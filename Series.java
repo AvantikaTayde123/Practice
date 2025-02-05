b3f750fb36a54a567b840499dae5c9978eed4670
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-02-05 23:47:10 +0530

    Create Series.java

---
+++ Series.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int n=10, diff=0;
+    for(int i=0;i<=100;i++){
+        n=n+diff;
+        diff++;
+        System.out.println(n+" ");
+    }
+    
+    }
+}
+//o/p 10,11,13,16,20,25,31--------------1780//100th term
